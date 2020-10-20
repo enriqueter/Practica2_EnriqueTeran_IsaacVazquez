ab9d0dd0e35e819fe518e91b85a7671a5454ed67
isaacvg92 <31740180+isaacvg92@users.noreply.github.com>
2020-10-19 23:04:29 -0500

    header completed

--- AHRS.h
+++ AHRS.h
@@ -1,42 +1,39 @@
-/*
- * AHRS.h
- *
- *  Created on: 19 oct. 2020
- *      Author: etera
- */
-
 #ifndef AHRS_H_
 #define AHRS_H_
-
 #include "BMI160.h"
 #include "rtos_uart.h"
-#include "Mahony.h"
+#include "mahony.h"
 
-#define Accelerometer_range 2 /*Accelerometer data range*/
-#define Gyroscope_range 2000 /*Gryoscope data range*/
-#define MAX_VAL 32768 /*BMI160 MAX VALUE*/
-#define AHRS_IMU_TIME 	10 /* Delay time for IMU read data*/
+#define A_range 2 //2g 
+#define G_range 2000	// 2000°/s 
+#define MAX_VAL 32768 //INT16
+#define HEADER 0xAAAAAAAA
+#define AHRS_IMU_TIME 	10
+#define AHRS_UART_TIME 	5
+#define SAMPLES 10
 
-
+typedef struct {
+	uint32_t header;
+	float x;
+	float y;
+	float z;
+} comm_msg_t;
 
 typedef struct {
 	float x;
 	float y;
 	float z;
-}fAccel_data_t;
+}BMI160_fA_data_t;
 
 typedef struct {
 	float x;
 	float y;
 	float z;
-}fGyro_data_t;
+}BMI160_fG_data_t;
 
-/*Function to get raw data from BMI160*/
-void get_BMI160_data(void * args);
-/*Function to make conversion of the raw data from the accelerometer*/
-void Accel_conversion(void);
-/*Function to make conversion of the raw data from the gryoscope*/
-void Gyroscope_conversion(void);
-
+void AHRS_UART(void * args);
+void get_data(void * args);
+void A_conversion(void);	// Conversion
+void G_conversion(void);	// Conversion
 
 #endif /* AHRS_H_ */
